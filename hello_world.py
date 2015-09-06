9079d7a85961d48760da86ac43a1403a1409d69e
mdbernat1 <mdbernat1@msn.com>
2015-09-06 12:49:45 -0500

    Create hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+def encode( input ):
+  onestr = ""
+  for item in input:
+      if item.isalnum() == False:
+        print "%s is not a valid input" % item
+        exit();
+      onestr = onestr + item + "*"
+  return onestr[:-1];
+  
+def decode( onestring ):
+  return onestring.split("*");
+  
+input = ["dwe","qwdw","cwewre"];
+print "input: ", input
+onestring = encode(input);
+print "after encode: ", onestring
+output = decode(onestring);
+print "output: ", output
